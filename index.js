cb50c8639eeb7986f513384ff2dafb542a232cda
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-08-23 23:11:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names){
+    let cards = [];
+    for (let i = 0; i < names.length; i++) {
+       cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+        
+    }
+    return cards ;
+}
+function countDown(num){
+    let n = num;
+    while(n >= 0){
+        console.log(n--)
+    }
+}
